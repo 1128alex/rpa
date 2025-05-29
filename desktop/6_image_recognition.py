6c8e979132264564286819d040c0befb4f3e3c05
Alex Lee <1128alex@gmail.com>
2025-05-30 02:37:55 +0900

    rpa study

--- desktop/6_image_recognition.py
+++ desktop/6_image_recognition.py
@@ -1,4 +1,6 @@
 import pyautogui
+from pyautogui import ImageNotFoundException
+
 
 # locateOnScreen(): find the image from current screen
 # file_menu = pyautogui.locateOnScreen("file_menu.png")
@@ -32,4 +34,57 @@ import pyautogui
 # pyautogui.moveTo(trash_icon)
 
 
+def safe_locate_on_screen(*args, **kwargs):
+    try:
+        return pyautogui.locateOnScreen(*args, **kwargs)
+    except ImageNotFoundException:
+        return None
+
+
 # when the automation component is not shown right away
+# # 1. Wait until the image is shown
+# file_menu_notepad = safe_locate_on_screen("file_menu_notepad.png")
+# while file_menu_notepad is None:
+#     file_menu_notepad = safe_locate_on_screen("file_menu_notepad.png")
+#     print("fail")
+
+# pyautogui.click(file_menu_notepad)
+
+# 2. Wait for a certain time (Timeout)
+import time
+import sys
+
+timeout = 10
+start = time.time()
+file_menu_notepad = None
+while file_menu_notepad is None:
+    file_menu_notepad = safe_locate_on_screen("file_menu_notepad.png")
+    end = time.time()
+    if end - start > timeout:
+        print("시간 종료")
+        sys.exit()
+
+
+def find_target(img_file, timeout=30):
+    start = time.time()
+    target = None
+    while target is None:
+        target = pyautogui.locateOnScreen(img_file)
+        end = time.time()
+        if end - start > timeout:
+            break
+    return target
+
+
+def my_click(img_file, timeout=30):
+    target = find_target(img_file, timeout)
+    if target:
+        pyautogui.click(target)
+    else:
+        print("f[Timeout {timeout}s] Target not found ({img_file}). Terminate program.")
+        sys.exit()
+
+
+# pyautogui.click(file_menu_notepad)
+
+my_click("file_menu_notepad.png", 10)
